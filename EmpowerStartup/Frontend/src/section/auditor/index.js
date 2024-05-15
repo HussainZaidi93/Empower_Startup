c68ba4542e5cefa1c269b41ea311ddf481d31525
Waslah <151165442+Waslah@users.noreply.github.com>
2024-05-15 01:52:23 -0700

    Update index.js

--- EmpowerStartup/Frontend/src/section/auditor/index.js
+++ EmpowerStartup/Frontend/src/section/auditor/index.js
@@ -1,5 +1,6 @@
 export { default as AuditorHomeSection } from './HomePage';
 export { default as PlaceAudit } from './PlaceAudit';
 export { default as PlaceAuditDialog } from './PlaceAuditDialog';
+export { default as AuditProductsDialog } from './AuditProductsDialog';
 
 
